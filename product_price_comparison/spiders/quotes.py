1b4126f1a7b1128abc4c4acfd8d99e959f345318
TusharMazhar <tushar.talukdar@northsouth.edu>
2019-10-28 21:52:13 +0600

    project setup commit

--- product_price_comparison/spiders/quotes.py
+++ product_price_comparison/spiders/quotes.py
@@ -5,7 +5,9 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
+    
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
         pass
+
